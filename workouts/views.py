934cc3b12124f252eda5a204e82984cefcf4fe68
Markus Barth <markus@markusbarth.net>
2020-03-25 20:10:48 +0100

    added dropdown menus

--- workouts/views.py
+++ workouts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from .models import Workout, WorkoutExercise
+from .models import Workout, WorkoutExercise, WorkoutType
+from django.contrib.auth import get_user_model
 
 def index(request):
     context = {
-        'workoutlist': Workout.objects.order_by('name')
+        'workoutlist': Workout.objects.order_by('name'),
+        'workouttypelist': WorkoutType.objects.order_by('name'),
+        'workoutdifficultylist': Workout.DIFFICULTY_CHOICES,
+        'userlist': get_user_model().objects.order_by('last_name')
     }
     return render(request, 'workouts/index.html', context)
 
