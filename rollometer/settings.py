666b85341f3e4ec29c95ed757da70a8a8a43e08c
Markus Barth <markus@markusbarth.net>
2020-03-25 10:59:20 +0100

    changed debug mode

--- rollometer/settings.py
+++ rollometer/settings.py
@@ -6,6 +6,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_URL = 'rollometer'
 
 
 # Quick-start development settings - unsuitable for production
