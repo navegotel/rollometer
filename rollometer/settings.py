ae4e00159a7e6c74166c6b094208e98ea9086087
Markus Barth <markus@markusbarth.net>
2020-03-25 11:28:23 +0100

    changed debug mode

--- rollometer/settings.py
+++ rollometer/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/rollometer_static/'
+STATIC_URL = '/static/'
 
 try:
     from .local_settings import *
